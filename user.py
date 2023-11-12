f69460ae0ecc3b8071d3effaad2869463a2cc6eb
swooten4 <149175771+swooten4@users.noreply.github.com>
2023-11-12 16:52:15 -0600

    Add files via upload
    Began work on the user class! Login is implemented...more or less :)

---
+++ user.py
@@ -0,0 +1,42 @@
+##### user class ####
+import sqlite3
+import sys
+
+class user:
+    def __init__(self, databaseName = "", tableName = ""):
+        self.database = databaseName
+        self.table = tableName
+
+    def login(self):
+        email = input("What is your email? > ")
+        password = input("What is your password? > ")
+
+        try:
+            connection = sqlite3.connect(self.database)
+        except:
+            print("Connection to users database failed")
+            sys.exit()
+
+        cursor = connection.cursor()
+        cursor.execute(f'SELECT EXISTS(SELECT * FROM {self.table} WHERE Email = "{email}" AND Password = "{password}")')
+    
+        # Takes the resulting boolean out of the list of tuples it exists in.
+        isLoggedIn = cursor.fetchall()[0][0]
+
+        cursor.execute(f'SELECT UserID FROM {self.table} WHERE Email = "{email}" AND Password = "{password}"')
+        # Takes UserID out of the list of tuples and stores it as a class variable for later use!
+        self.UserID = cursor.fetchall()[0][0]
+
+        return isLoggedIn
+       
+    def logout(self):
+       print("temp")
+
+    def createAccount(self):
+        try:
+            connection = sqlite3.connect("shopping.db")
+        except:
+            print("Connection to users database failed")
+            sys.exit()
+
+        cursor = connection.cursor()
