b06c17beec401a75e557cd9ee2525961aa7ec0c1
swooten4 <149175771+swooten4@users.noreply.github.com>
2023-11-13 17:03:40 -0600

    Add files via upload
    Began work on a few more functions! Logout seems to be complete, and a bit of primitive work has been done on the "createAccount" function. Still need to check for appropriate inputs (ex. validating that the zip code entered is 5 digits), but the rough "making an entry" part works!

--- user.py
+++ user.py
@@ -7,7 +7,7 @@ class user:
         self.database = databaseName
         self.table = tableName
         self.UserID = 0
-        self.IsLoggedIn = 0
+        self.isLoggedIn = 0
 
     def login(self):
         email = input("What is your email? > ")
@@ -26,8 +26,9 @@ class user:
         self.isLoggedIn = cursor.fetchall()[0][0]
 
         # Prevents an error when getting the UserID. After all, if the user does not exist,
-        # how are you supposed to fetch their ID?
+        # how are you supposed to fetch their ID? Stop here before that happens!
         if(self.isLoggedIn == 0):
+            print("User does not exist. Check email and password.")
             return self.isLoggedIn
 
         cursor.execute(f'SELECT UserID FROM {self.table} WHERE Email = "{email}" AND Password = "{password}"')
@@ -39,7 +40,10 @@ class user:
         return self.isLoggedIn
        
     def logout(self):
-       print("temp")
+       self.UserID = 0
+       self.isLoggedIn = 0
+
+       return self.isLoggedIn
 
     def createAccount(self):
         try:
@@ -49,3 +53,19 @@ class user:
             sys.exit()
 
         cursor = connection.cursor()
+
+        userID = 0
+        Email = input("What is your email? > ")
+        Password = input("What is your password? > ")
+        FirstName = input("What is your first name? > ")
+        LastName = input("What is your last name? > ")
+        Address = input("What is your address? > ")
+        City = input("What city do you live in? > ")
+        State = input("What state do you live in? > ")
+        Zip = int(input("What is your zip code? > "))
+        Payment = int(input("What is your credit card number (Enter 16 digits with no spaces) > "))
+
+        cursor.execute(f'INSERT INTO user (UserID, Email, Password, FirstName, LastName, Address, City, State, Zip, Payment) VALUES ("{userID}", "{Email}", "{Password}", "{FirstName}", "{LastName}", "{Address}", "{City}", "{State}", "{Zip}", "{Payment}")')
+        connection.commit()
+
+        connection.close()
