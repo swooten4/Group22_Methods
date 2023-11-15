ee45f6ceabc9f83c671333a09165fa2df6f7dab7
swooten4 <149175771+swooten4@users.noreply.github.com>
2023-11-14 19:41:40 -0600

    Add files via upload
    Here we have a jussst about completed user class. Still needs testing.

--- user.py
+++ user.py
@@ -11,6 +11,7 @@ class user:
 
 
     def login(self):
+        print("-_-_- Log In -_-_-")
         email = input("What is your email? > ")
         password = input("What is your password? > ")
 
@@ -33,7 +34,6 @@ class user:
             return self.isLoggedIn
 
         cursor.execute(f'SELECT UserID FROM {self.table} WHERE Email = "{email}" AND Password = "{password}"')
-        # Takes UserID out of the list of tuples and stores it as a class variable for later use!
         self.UserID = cursor.fetchall()[0][0]
 
         connection.close()
@@ -57,22 +57,43 @@ class user:
 
         cursor = connection.cursor()
 
-        userID = 0
+        # How I am handling UserID: literally just add 1 to the number of rows.
+        # i.e. the first user is "1" and so on and so forth.
+        cursor.execute("SELECT COUNT(UserID) FROM user")
+        numRows = cursor.fetchall()
+        userID = numRows[0][0] + 1
+
+        # Email needs to be unique so that login can function properly. Plus, if they already have an
+        # account, why let them make another?
         Email = input("What is your email? > ")
+        cursor.execute(f'SELECT EXISTS(SELECT UserID from user WHERE Email = "{Email}")')
+        isUnique = cursor.fetchall()[0][0]
+        while(isUnique != 0):
+            Email = input("A user exists with that email! Please input a different email! > ")
+            cursor.execute(f'SELECT EXISTS(SELECT UserID from user WHERE Email = "{Email}")')
+            isUnique = cursor.fetchall()[0][0]
+
         Password = input("What is your password? > ")
         FirstName = input("What is your first name? > ")
         LastName = input("What is your last name? > ")
         Address = input("What is your address? > ")
         City = input("What city do you live in? > ")
         State = input("What state do you live in? > ")
+
         Zip = int(input("What is your zip code? > "))
+        while (len(str(Zip)) != 5):
+            Zip = int(input("There should be 5 digits in a zip code. Please try again > "))
 
         Payment = int(input("What is your credit card number (Enter 16 digits with no spaces) > "))
-        while (len(str(Payment)) < 16 or len(str(Payment)) > 16):
+        while (len(str(Payment)) != 16):
             Payment = int(input("There should be 16 digits. Please try again > "))
 
 
-        cursor.execute(f'INSERT INTO user (UserID, Email, Password, FirstName, LastName, Address, City, State, Zip, Payment) VALUES ("{userID}", "{Email}", "{Password}", "{FirstName}", "{LastName}", "{Address}", "{City}", "{State}", "{Zip}", "{Payment}")')
+        cursor.execute(f'INSERT INTO user (UserID, Email, Password, FirstName,\
+                        LastName, Address, City, State, Zip, Payment)\
+                        VALUES ("{userID}", "{Email}", "{Password}", "{FirstName}",\
+                        "{LastName}", "{Address}", "{City}", "{State}", "{Zip}", "{Payment}")')
+
         connection.commit()
 
         connection.close()
@@ -88,6 +109,7 @@ class user:
         cursor = connection.cursor()
 
         try:
+            # Thank goodness this query returns data in the same order every time. 
             cursor.execute(f'SELECT * FROM user WHERE UserID = "{self.UserID}"')
             results = cursor.fetchall()
             print(f'UserID: {results[0][0]}')
@@ -105,7 +127,16 @@ class user:
 
         connection.close()
 
+    def getUserID(self):
+        return self.UserID
 
+    def getLoggedIn(self):
+        return self.isLoggedIn
+
+
+
+
+    # DELETE LATER. Hahah delete the delete accout function. There's an irony in that.
     def deleteAccount(self):
         try:
             connection = sqlite3.connect("shopping.db")
