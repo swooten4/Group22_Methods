1c7b713c4ba83e49af8a1f115ed6c295e5e601c8
Reagan-Storey <149591995+Reagan-Storey@users.noreply.github.com>
2023-11-28 21:02:25 -0600

    Add files via upload

--- Inventory.py
+++ Inventory.py
@@ -1,14 +1,21 @@
 import sqlite3
+import sys
 
 class BookInventory:
     def __init__(self, databaseName = "", tableName = ""):
         self.database = databaseName
         self.table = tableName
-        self.connection = sqlite3.connect(self.databaseName)
-        self.cursor = self.connection.cursor()
+    
 
 
     def view_inventory(self):
+
+        try:
+            connection = sqlite3.connect(self.database)
+        except:
+            print("Connection to users database failed")
+            sys.exit()
+        
         query = f"SELECT * FROM {self.tableName}"
         self.cursor.execute(query)
         rows = self.cursor.fetchall()
@@ -16,6 +23,13 @@ class BookInventory:
             print(row)
 
     def search_inventory(self, isbn):
+
+        try:
+            connection = sqlite3.connect(self.database)
+        except:
+            print("Connection to users database failed")
+            sys.exit()
+
         query = f"SELECT * FROM {self.tableName} WHERE ISBN = ?"
         self.cursor.execute(query, (isbn,))
         row = self.cursor.fetchone()
@@ -25,6 +39,13 @@ class BookInventory:
             print("Book not found in inventory.")
 
     def decrease_stock(self, isbn):
+
+        try:
+            connection = sqlite3.connect(self.database)
+        except:
+            print("Connection to users database failed")
+            sys.exit()
+
         query_check_stock = f"SELECT Stock FROM {self.tableName} WHERE ISBN = ?"
         self.cursor.execute(query_check_stock, (isbn,))
         current_stock = self.cursor.fetchone()
