ee29c3edd87e93835e98bbaf47a180a274612c87
Reagan-Storey <149591995+Reagan-Storey@users.noreply.github.com>
2023-11-28 20:27:45 -0600

    Add files via upload

--- Inventory.py
+++ Inventory.py
@@ -1,63 +1,46 @@
-class Inventory:
-    def __init__(self):
-        self.inventory = {}
+import sqlite3
 
-    def add_book(self, isbn, title, author, genre, pages, release_date, stock, price):
-        if isbn in self.inventory:
-            print(f"Book with ISBN {isbn} already exists in the inventory.")
+class BookInventory:
+    def __init__(self, databaseName = "", tableName = ""):
+        self.database = databaseName
+        self.table = tableName
+        self.connection = sqlite3.connect(self.databaseName)
+        self.cursor = self.connection.cursor()
+
+
+    def view_inventory(self):
+        query = f"SELECT * FROM {self.tableName}"
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+        for row in rows:
+            print(row)
+
+    def search_inventory(self, isbn):
+        query = f"SELECT * FROM {self.tableName} WHERE ISBN = ?"
+        self.cursor.execute(query, (isbn,))
+        row = self.cursor.fetchone()
+        if row:
+            print(row)
         else:
-            self.inventory[isbn] = {
-                'Title': title,
-                'Author': author,
-                'Genre': genre,
-                'Pages': pages,
-                'ReleaseDate': release_date,
-                'Stock': stock,
-                'Price': price
-            }
-            print(f"Book with ISBN {isbn} added to the inventory.")
+            print("Book not found in inventory.")
 
-    def get_book_info(self, isbn):
-        if isbn in self.inventory:
-            return self.inventory[isbn]
+    def decrease_stock(self, isbn):
+        query_check_stock = f"SELECT Stock FROM {self.tableName} WHERE ISBN = ?"
+        self.cursor.execute(query_check_stock, (isbn,))
+        current_stock = self.cursor.fetchone()
+
+        if current_stock:
+            current_stock = current_stock[0]
+            if current_stock > 0:
+                new_stock = current_stock - 1
+                query_update_stock = f"UPDATE {self.tableName} SET Stock = ? WHERE ISBN = ?"
+                self.cursor.execute(query_update_stock, (new_stock, isbn))
+                self.connection.commit()
+                print(f"Stock for {isbn} decreased. New stock: {new_stock}")
+            else:
+                print(f"Sorry, {isbn} is out of stock.")
         else:
-            print(f"Book with ISBN {isbn} not found in the inventory.")
-            return None
+            print("Book not found in inventory.")
 
-    def update_stock(self, isbn, new_stock):
-        if isbn in self.inventory:
-            self.inventory[isbn]['Stock'] = new_stock
-            print(f"Stock for book with ISBN {isbn} updated to {new_stock}.")
-        else:
-            print(f"Book with ISBN {isbn} not found in the inventory.")
-
-    def remove_book(self, isbn):
-        if isbn in self.inventory:
-            del self.inventory[isbn]
-            print(f"Book with ISBN {isbn} removed from the inventory.")
-        else:
-            print(f"Book with ISBN {isbn} not found in the inventory.")
-
-    def get_all_books(self):
-        return self.inventory
-
-# Example Usage:
-inventory = Inventory()
-
-# Add books to the inventory
-inventory.add_book(123456789, "Sample Book 1", "John Doe", "Fiction", "300", "2023-01-01", 10, 19.99)
-inventory.add_book(987654321, "Sample Book 2", "Jane Smith", "Non-Fiction", "250", "2023-02-01", 15, 29.99)
-
-# Get information about a specific book
-book_info = inventory.get_book_info(123456789)
-print("Book Info:", book_info)
-
-# Update stock for a book
-inventory.update_stock(123456789, 15)
-
-# Remove a book from the inventory
-inventory.remove_book(987654321)
-
-# Get information about all books in the inventory
-all_books = inventory.get_all_books()
-print("All Books:", all_books)
+    def __del__(self):
+        self.connection.close()
